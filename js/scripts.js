994f6031fdd00bb7b4d56158f8043162e5bba3fa
Woody Lin <wancheng.lin001@umb.edu>
2023-04-02 23:40:21 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,100 +13,104 @@ function showDataset(evt, cityName) {
 }
 
 function fetchJSONFile(path, callback) {
-var httpRequest = new XMLHttpRequest();
-httpRequest.onreadystatechange = function () {
-  if (httpRequest.readyState === 4) {
-    if (httpRequest.status === 200) {
-      var data = JSON.parse(httpRequest.responseText);
-      if (callback) callback(data);
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function () {
+    // Check if the request is complete and the status is OK
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        var data = JSON.parse(httpRequest.responseText);
+        if (callback) callback(data);
+      }
     }
-  }
-};
-httpRequest.open("GET", path);
-httpRequest.send();
+  };
+  httpRequest.open("GET", path);
+  httpRequest.send();
 }
 
+// Global leaderboard data array
 var leaderboardData = [];
 
+// Fetch JSON data and initialize the leaderboard after the data is loaded
 fetchJSONFile("json/example-data.json", function (data) {
-leaderboardData = data;
+  leaderboardData = data;
 
-// Initialize the leaderboard with default sorting and highlighting
-updateLeaderboard("All_Datasets");
-highlightColumn("All_Datasets");
+  // Initialize the leaderboard with default sorting and highlighting
+  updateLeaderboard("All_Datasets");
+  highlightColumn("All_Datasets");
 });
 
 const datasetSelect = document.getElementById("dataset");
 const leaderboardTable = document.querySelector("tbody");
 
 function createTableCell(content) {
-const cell = document.createElement("td");
-cell.textContent = content;
-return cell;
+  const cell = document.createElement("td");
+  cell.textContent = content;
+  return cell;
 }
 
 function updateLeaderboard(selectedDataset) {
-leaderboardTable.innerHTML = "";
-let sortedData;
+  leaderboardTable.innerHTML = "";
+  let sortedData;
 
-if (selectedDataset === "All_Datasets") {
-  sortedData = leaderboardData.slice().sort((a, b) => {
-    const aTotal = a.dataset1 + a.dataset2 + a.dataset3;
-    const bTotal = b.dataset1 + b.dataset2 + b.dataset3;
-    return bTotal - aTotal;
+// Sort data based on the selected dataset
+  if (selectedDataset === "All_Datasets") {
+      sortedData = leaderboardData.slice().sort((a, b) => {
+      const aTotal = a.dataset1 + a.dataset2 + a.dataset3;
+      const bTotal = b.dataset1 + b.dataset2 + b.dataset3;
+      return bTotal - aTotal;
+    });
+  } else {
+    sortedData = leaderboardData.slice().sort((a, b) => b[selectedDataset] - a[selectedDataset]);
+  }
+
+  sortedData.forEach((entry) => {
+    const row = document.createElement("tr");
+
+    row.appendChild(createTableCell(entry.team));
+    row.appendChild(createTableCell(entry.dataset1.toFixed(2)));
+    row.appendChild(createTableCell(entry.dataset2.toFixed(2)));
+    row.appendChild(createTableCell(entry.dataset3.toFixed(2)));
+
+    leaderboardTable.appendChild(row);
   });
-} else {
-  sortedData = leaderboardData.slice().sort((a, b) => b[selectedDataset] - a[selectedDataset]);
-}
-
-sortedData.forEach((entry) => {
-  const row = document.createElement("tr");
-
-  row.appendChild(createTableCell(entry.team));
-  row.appendChild(createTableCell(entry.dataset1.toFixed(2)));
-  row.appendChild(createTableCell(entry.dataset2.toFixed(2)));
-  row.appendChild(createTableCell(entry.dataset3.toFixed(2)));
-
-  leaderboardTable.appendChild(row);
-});
 }
 
 function highlightColumn(selectedDataset) {
-const thElements = document.querySelectorAll("th");
-const rows = document.querySelectorAll("tbody tr");
+  const thElements = document.querySelectorAll("th");
+  const rows = document.querySelectorAll("tbody tr");
 
-thElements.forEach((thElement, index) => {
-  if (index > 0 && thElements[index].classList.contains("highlighted")) {
-    thElements[index].classList.remove("highlighted");
-  }
-});
-
-rows.forEach((row) => {
-  row.childNodes.forEach((cell, index) => {
-    if (index > 0 && cell.classList.contains("highlighted")) {
-      cell.classList.remove("highlighted");
+  thElements.forEach((thElement, index) => {
+    if (index > 0 && thElements[index].classList.contains("highlighted")) {
+      thElements[index].classList.remove("highlighted");
     }
   });
-});
 
-switch (selectedDataset) {
-  case "dataset1":
-    thElements[1].classList.add("highlighted");
-    rows.forEach((row) => row.childNodes[1].classList.add("highlighted"));
-    break;
-  case "dataset2":
-    thElements[2].classList.add("highlighted");
-    rows.forEach((row) => row.childNodes[2].classList.add("highlighted"));
-    break;
-  case "dataset3":
-    thElements[3].classList.add("highlighted");
-    rows.forEach((row) => row.childNodes[3].classList.add("highlighted"));
-    break;
-}
+  rows.forEach((row) => {
+    row.childNodes.forEach((cell, index) => {
+      if (index > 0 && cell.classList.contains("highlighted")) {
+        cell.classList.remove("highlighted");
+      }
+    });
+  });
+
+  switch (selectedDataset) {
+    case "dataset1":
+      thElements[1].classList.add("highlighted");
+      rows.forEach((row) => row.childNodes[1].classList.add("highlighted"));
+      break;
+    case "dataset2":
+      thElements[2].classList.add("highlighted");
+      rows.forEach((row) => row.childNodes[2].classList.add("highlighted"));
+      break;
+    case "dataset3":
+      thElements[3].classList.add("highlighted");
+      rows.forEach((row) => row.childNodes[3].classList.add("highlighted"));
+      break;
+  }
 }
 
 datasetSelect.addEventListener("change", (event) => {
-const selectedDataset = event.target.value;
-updateLeaderboard(selectedDataset);
-highlightColumn(selectedDataset);
+  const selectedDataset = event.target.value;
+  updateLeaderboard(selectedDataset);
+  highlightColumn(selectedDataset);
 });
