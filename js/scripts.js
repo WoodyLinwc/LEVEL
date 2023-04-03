9ef239a3d4224c860768b5e45f18dfc07e7de7c3
Woody Lin <wancheng.lin001@umb.edu>
2023-04-02 23:51:49 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function showDataset(evt, cityName) {
+function showDataset(evt, DatasetName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -8,7 +8,7 @@ function showDataset(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(DatasetName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
