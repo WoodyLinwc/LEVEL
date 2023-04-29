90fd337ab15cf770a4a0dca8e5518db36527f045
Woody Lin <wancheng.lin001@umb.edu>
2023-04-28 23:37:22 -0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,1 +1,31 @@
-print("eval script")
+import csv
+import json
+
+def read_csv_file(file_name):
+    data = []
+    with open(file_name, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
+
+def evaluate_data():
+    # Add your evaluation logic here and update the json_output accordingly
+    json_output = [
+        {
+            "team": "Team D",
+            "dataset1": 0.21,
+            "dataset2": 0.45,
+            "dataset3": 0.55
+        }
+    ]
+    return json_output
+
+def write_json_file(data, file_name):
+    with open(file_name, 'w') as json_file:
+        json.dump(data, json_file, indent=2)
+
+if __name__ == "__main__":
+    json_output = evaluate_data()
+
+    write_json_file(json_output, "new.json")
